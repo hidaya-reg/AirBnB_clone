d9d88b973f67f979044eea17fcc5929d99a4c576
hidaya <hidaya.regragui@gmail.com>
2023-11-11 21:07:27 -0800

    task0

---
+++ tests/test_base_model.py
@@ -0,0 +1,15 @@
+""" Testing module: base_model"""
+from models.base_model import BaseModel
+import unittest
+
+class TestBaseModel(unittest.TestCase):
+    """Testing module: base_model"""
+
+    def test_str(self):
+        """ Test str representation"""
+        b = BaseModel()
+        s = f"[{type(b).__name__}] ({b.id}) {b.__dict__}"
+        self.assertEqual(b.__str__(), s)
+
+if __name__ == "__main__":
+    unittest.main()
