d31428909ccebe9f09b95bfdd9aa1a560a8e0cce
hidaya <hidaya.regragui@gmail.com>
2023-11-11 21:32:07 -0800

    __init__.py files

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+__init__.py file to intialize importing package
+"""
+
+from models.base_model import BaseModel
--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ class BaseModel
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
