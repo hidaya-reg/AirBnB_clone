0bf72796d30e53550f2f5164c198c0f89f4372f1
hidaya <hidaya.regragui@gmail.com>
2023-11-14 10:41:18 -0800

    airbnb clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,20 +34,30 @@ class FileStorage():
 
     def save(self):
         """Serializes __objects to the JSON file."""
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
             json.dump(
                 {k: v.to_dict() for k, v in self.__objects.items()}, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
-        if path.exists(self.__file_path):
-            with open(self.__file_path, mode='r', encoding='utf-8') as file:
-                data = json.load(file)
-                for key, obj_data in data.items():
-                    cls_name, obj_id = key.split('.')
-                    obj_data['created_at'] = datetime.fromisoformat(
-                        obj_data['created_at'])
-                    obj_data['updated_at'] = datetime.fromisoformat(
-                        obj_data['updated_at'])
-                    instance = eval(cls_name)(**obj_data)
-                    self.__objects[key] = instance
+        """Deserializes the JSON file"""
+        current_classes = {'BaseModel': BaseModel, 'User': User,
+                           'Amenity': Amenity, 'City': City, 'State': State,
+                           'Place': Place, 'Review': Review}
+
+        if not path.exists(self.__file_path):
+            return
+
+        with open(self.__file_path, 'r', encoding='utf-8') as f:
+            deserialized = None
+
+            try:
+                deserialized = json.load(f)
+            except json.JSONDecodeError:
+                pass
+
+            if deserialized is None:
+                return
+
+            self.__objects = {
+                k: current_classes[k.split('.')[0]](**v)
+                for k, v in deserialized.items()}
